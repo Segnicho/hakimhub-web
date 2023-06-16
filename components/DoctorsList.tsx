7fe7797a310d7339a41f7cdc79e5660846bbc053
Ekram <ekruiwc2020@gmail.com>
2023-06-16 11:55:31 +0300

    fetch and display doctors in a specific hospital

---
+++ components/DoctorsList.tsx
@@ -0,0 +1,67 @@
+import DoctorCard from './DoctorCard';
+import doctors from './../public/data/doctor.json';
+
+const DoctorsList = () => {
+  return (
+    <div className='mx-6 my-6'>
+      <div className='flex flex-row'>
+        <h3 className='text-2xl font-bold mx-20'>Doctors at Tikur Ambessa</h3>
+        <div className='flex flex-row ml-36'>
+          <div className='relative'>
+            <label htmlFor='speciality' className='text-md font-semi-bold text-blue-500'>
+              Speciality
+            </label>
+            <select
+              id='speciality'
+              className='block w-32 mt-1 py-2 px-3 bg-white rounded-md  drop-shadow shadow-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500'
+            >
+              <option value=''>Cardiologist</option>
+              <option value='Cardiologist'>Cardiologist</option>
+              <option value='Dermatologist'>Dermatologist</option>
+              <option value='Gynecologist'>Gynecologist</option>
+              <option value='Pediatrician'>Pediatrician</option>
+            </select>
+          </div>
+          <div className='relative ml-4'>
+            <label htmlFor='university' className='text-md font-semi-bold text-blue-500'>
+              Education
+            </label>
+            <select
+              id='university'
+              className='block w-48 mt-1 py-2 px-3 bg-white rounded-md  drop-shadow shadow-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500'
+            >
+              <option value=''>Addis Ababa University</option>
+              <option value='Addis Ababa University'>Addis Ababa University</option>
+              <option value='Hawassa University'>Hawassa University</option>
+              <option value='Mekelle University'>Mekelle University</option>
+              <option value='Jimma University'>Jimma University</option>
+              <option value='Bahir Dar University'>Bahir Dar University</option>
+            </select>
+          </div>
+          <div className='relative ml-4'>
+            <label htmlFor='experience' className='text-md font-semi-bold text-blue-500'>
+              Experience
+            </label>
+            <select
+              id='experience'
+              className='block w-32 mt-1 py-2 px-3 bg-white rounded-md  drop-shadow shadow-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500'
+            >
+              <option value=''>0-5 years</option>
+              <option value='0-5 years'>0-5 years</option>
+              <option value='5-10 years'>5-10 years</option>
+              <option value='10-15 years'>10-15 years</option>
+              <option value='15+ years'>15+ years</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div className='flex flex-wrap mx-4 my-20'>
+        {doctors.Doctors.map((doctor, index) => (
+          <DoctorCard key={index} doctor={doctor} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DoctorsList;
