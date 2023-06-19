cac15516133b1d1321c6787268135ef47c73e10d
Tseganesh Yifru <tseganeshyifruberta>
2023-06-19 11:42:37 +0300

    rebase main

--- pages/hospital/index.tsx
+++ pages/hospital/index.tsx
@@ -7,7 +7,7 @@ export default function HospitalsPage() {
   return (
     <div className="bg-primary-bg">
       <div className="flex flex-wrap">
-        <div className="w-3/4 p-16 max-lg:w-full">
+        <div className="w-3/4 p-16 pt-0 max-lg:w-full">
           <SearchCard />
           <HospitalsList />
         </div>
