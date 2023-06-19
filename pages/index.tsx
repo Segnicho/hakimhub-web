f0cce4e601ed492b08bf35330ffb0affde31047f
Segnicho <segnidesta60@gmail.com>
2023-06-19 15:49:30 +0300

    remove font to fix deployment issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { phone } from "@/public";
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col px-10 `}>
+    <main className={`flex min-h-screen flex-col px-10`}>
       <Hero />
       <Search />
       <div className="mt-10">
