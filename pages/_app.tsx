9367dac071a01cd185469d76d0e373b64a423998
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-06-20 11:01:37 +0300

    add provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import RootLayout from "@/layouts/RootLayout";
 import "@/styles/globals.css";
+import {store} from "@/store"
 import type { AppProps } from "next/app";
-
+import {Provider}  from "react-redux"
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Provider store={store}>
       <RootLayout>
         <Component {...pageProps} />
       </RootLayout>
+        </Provider>
     </>
   );
 }
