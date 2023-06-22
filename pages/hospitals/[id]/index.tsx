510665454b18a9afb759a6b62c7db24571b29cfd
Ekram <ekruiwc2020@gmail.com>
2023-06-23 01:37:03 +0300

    made the necessary changes to ensure responsiveness in the code and implemented a hamburger menu for small-sized screens

--- pages/hospitals/[id]/index.tsx
+++ pages/hospitals/[id]/index.tsx
@@ -14,6 +14,7 @@ const HospitalDetailPage = () => {
   const id = router.query.id as string;
   const { data: hospital, isSuccess, isLoading, isError, error } = useGetHospitalByIdQuery(id);
   const [activeTab, setActiveTab] = useState('');
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   useEffect(() => {
     if (router.query.tab === 'doctors') {
@@ -29,56 +30,138 @@ const HospitalDetailPage = () => {
     router.push(`/hospitals/${id}?tab=${tab}`);
   };
 
-  if (isLoading) {
-    // Render loading state
-  }
+  const handleToggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
-  if (isSuccess) {
-    return (
-      <div className="grid grid-cols-3 grid-rows-1 bg-primary-bg">
-        <div className="w-30">
-          <HospitalDetailCard
-            image={hospital?.value.bannerUrl}
-            logo={hospital?.value.logoUrl}
-            status={hospital?.value.status}
-            name={hospital?.value.institutionName}
-          />
-          <ContactCard
-            website={hospital?.value.website}
-            phoneNumber={hospital?.value.phoneNumber}
-          />
-        </div>
-        <div className="row-span-3 col-span-2 flex-row mt-10 mr-10">
-          <nav className="grid grid-rows-1 grid-cols-3 w-full h-10 bg-white rounded-lg drop-shadow-md r-10 text-gray-500">
-            <button
-              onClick={() => handleTabClick('overview')}
-              className={`${
-                activeTab === 'overview' ? 'active' : ''
-              } hover:text-primary hover:border-b-2 hover:border-primary hover:mx-2`}
-            >
-              Overview
-            </button>
-            <button
-              onClick={() => handleTabClick('doctors')}
-              className={`${
-                activeTab === 'doctors' ? 'active' : ''
-              } hover:text-primary hover:border-b-2 hover:border-primary hover:mx-2`}
-            >
-              Doctors
-            </button>
-            <button
-              onClick={() => handleTabClick('gallery')}
-              className={`${
-                activeTab === 'gallery' ? 'active' : ''
-              } hover:text-primary hover:border-b-2 hover:border-primary hover:mx-2`}
-            >
-              Gallery
-            </button>
-          </nav>
+  const isMobileOrTablet = () => {
+    const windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    return windowWidth < 940; // Adjust the breakpoint as per your requirements
+  };
 
+  return (
+    <div className="sm:flex flex-col lg:grid lg:grid-cols-3 lg:grid-rows-1 grid grid-cols-3 grid-rows-1 bg-primary-bg">
+      <div className="w-30">
+        <HospitalDetailCard image={hospital?.value.bannerUrl} logo={hospital?.value.logoUrl} status={hospital?.value.status} name={hospital?.value.institutionName} />
+        <ContactCard website={hospital?.value.website} phoneNumber={hospital?.value.phoneNumber} />
+      </div>
+
+      <div className="mt-10 mx-20 lg:row-span-3 lg:col-span-2 lg:flex-row lg:mr-10">
+        <nav className="flex flex-wrap w-full bg-white rounded-lg text-gray-500 drop-shadow shadow-md lg:grid lg:grid-rows-1 lg:grid-cols-3 lg:w-full lg:h-10 lg:r-20">
+          <div className="flex items-center justify-between">
+            <div>
+              {isMobileOrTablet() ? (
+                <button
+                  className="text-gray-500 flex flex-row"
+                  onClick={handleToggleMobileMenu}
+                >                 
+                  <svg
+                    className={`mt-2 w-6 h-6 cursor-pointer ${
+                      isMobileMenuOpen ? "-mt-8" : ""
+                    }`}
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    {isMobileMenuOpen ? (
+                     
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M16 14H4V12H16V14ZM16 9H4V7H16V9ZM16 4H4V2H16V4Z"
+                        className='-mt-2'
+                      />
+                    
+                    ) : (
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M4 12H16V10H4V12ZM4 7H16V5H4V7ZM4 2H16V0H4V2Z"
+                      />
+                    )}
+                  </svg>
+                  <span className={isMobileMenuOpen ? 'hidden' : 'text-black ml-4'}>Menu</span>
+                </button>
+              ) : (
+                ''
+              )}
+            </div>
+          
+          </div>
+          
+          {isMobileOrTablet() && isMobileMenuOpen ? (
+            <div className="flex flex-col items-start mx-2">
+              <button
+                onClick={() => {
+                  handleTabClick('overview');
+                  handleToggleMobileMenu();
+                }}
+                className={`${
+                  activeTab === 'overview' ? 'active' : ''
+                } hover:text-primary hover:border-b-2 hover:border-primary hover:font-bold`}
+              >
+                Overview
+              </button>
+              <button
+                onClick={() => {
+                  handleTabClick('doctors');
+                  handleToggleMobileMenu();
+                }}
+                className={`${
+                  activeTab === 'doctors' ? 'active' : ''
+                } hover:text-primary hover:border-b-2 hover:border-primary hover:font-bold`}
+              >
+                Doctors
+              </button>
+              <button
+                onClick={() => {
+                  handleTabClick('gallery');
+                  handleToggleMobileMenu();
+                }}
+                className={`${
+                  activeTab === 'gallery' ? 'active' : ''
+                } hover:text-primary hover:border-b-2 hover:border-primary hover:font-bold`}
+              >
+                Gallery
+              </button>
+              
+            </div>
+          ) : (
+            !isMobileOrTablet() && (
+              <div className="lg:flex lg:flex-row lg:gap-40 lg:-ml-36">
+
+                <button
+                  onClick={() => handleTabClick('overview')}
+                  className={`${
+                    activeTab === 'overview' ? 'active' : ''
+                  } hover:text-primary hover:border-b-2 hover:border-primary hover:font-bold`}
+                >
+                  Overview
+                </button>
+                <button
+                  onClick={() => handleTabClick('doctors')}
+                  className={`${
+                    activeTab === 'doctors' ? 'active' : ''
+                  } hover:text-primary hover:border-b-2 hover:border-primary hover:font-bold`}
+                >
+                  Doctors
+                </button>
+                <button
+                  onClick={() => handleTabClick('gallery')}
+                  className={`${
+                    activeTab === 'gallery' ? 'active' : ''
+                  } hover:text-primary hover:border-b-2 hover:border-primary hover:font-bold`}
+                >
+                  Gallery
+                </button>
+              </div>
+            )
+          )}
+        </nav>
+
+        <div className="mt-4">
           {activeTab === 'overview' && (
             <div>
-              <Overview />
+              <h2>Overview</h2>
             </div>
           )}
           {activeTab === 'doctors' && hospital?.value.doctors && (
@@ -92,12 +175,8 @@ const HospitalDetailPage = () => {
           )}
         </div>
       </div>
-    );
-  }
-
-  if (isError) {
-    return <div>Error: {"Unknown Error"}</div>; // Show an error message if there's an error
-  }
+    </div>
+  );
 };
 
 export default HospitalDetailPage;
