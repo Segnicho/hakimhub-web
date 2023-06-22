0f5bef0a2b9e07b72018ebde9400d6354404139f
Ekram <ekruiwc2020@gmail.com>
2023-06-22 16:37:17 +0300

    arrange the UI

--- components/doctor/DoctorCard.tsx
+++ components/doctor/DoctorCard.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image';
-import {Doctor} from '../../types/hospital';
+import {Doctor} from '@/types/hospital';
 
 interface Props {
   doctor: Doctor;
 }
-
 const DoctorCard: React.FC<Props> = ({ doctor }) => {
 
   return (
@@ -12,7 +11,7 @@ const DoctorCard: React.FC<Props> = ({ doctor }) => {
       <div className="absolute top-4 left-28 w-20 h-20 rounded-full overflow-hidden border-2 border-white">
         <Image src={doctor?.photoUrl} alt="doctor image" width={80} height={80} layout="responsive" className="object-cover" priority />
       </div>
-      <h3 className="text-xl text-primary font-bold mx-8 mt-32">{doctor?.fullName}</h3>
+      <h3 className="text-xl font-bold mx-8 mt-32">{doctor?.fullName}</h3>
       <div className="border-b border-gray-300 my-1"></div>
       <p className="text-xl text-gray-600 p-5 mx-16">{doctor?.specialities[0].Name}</p>
     </div>
--- pages/hospitals/[id]/index.tsx
+++ pages/hospitals/[id]/index.tsx
@@ -1,78 +1,12 @@
-<<<<<<< HEAD
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import {
   DoctorsList,
   GalleryCard,
   ContactCard,
-  HospitalCard,
+  HospitalDetailCard,
   Overview,
-} from "@/components";
-
-const HospitalDetailPage = () => {
-  const router = useRouter();
-
-  const handleOverviewClick = () => {
-    router.push("/hospitals/[id]", `/hospitals/${router.query.id}`);
-  };
-
-  const handleDoctorsClick = () => {
-    router.push(
-      "/hospitals/[id]/doctors",
-      `/hospitals/${router.query.id}/doctors`
-    );
-  };
-
-  const handleGalleryClick = () => {
-    router.push(
-      "/hospitals/[id]/gallery",
-      `/hospitals/${router.query.id}/gallery`
-    );
-  };
-
-  return (
-    <div className="grid grid-cols-3 grid-rows-1 bg-secondary-bg">
-      <div className="flex-row w-30">
-        <HospitalCard />
-        <div className="my-10">
-          <ContactCard />
-        </div>
-      </div>
-      <div className="row-span-3 col-span-2 flex-row mt-20">
-        <nav className="grid grid-rows-1 grid-cols-3 w-full h-10 bg-white rounded-lg drop-shadow-md r-10 text-gray-500">
-          <button
-            onClick={handleOverviewClick}
-            className="hover:border-b-2 hover:border-blue-500 hover:text-blue-500 hover:font-extrabold"
-          >
-            Overview
-          </button>
-          <button
-            onClick={handleDoctorsClick}
-            className="hover:border-b-2 hover:border-blue-500 hover:text-blue-500 hover:font-extrabold"
-          >
-            Doctors
-          </button>
-          <button
-            onClick={handleGalleryClick}
-            className="hover:border-b-2 hover:border-blue-500 hover:text-blue-500 hover:font-extrabold"
-          >
-            Gallery
-          </button>
-        </nav>
-
-        {router.pathname === "/hospitals/[id]" && <Overview />}
-        {router.pathname === "/hospitals/[id]/doctors" && <DoctorsList />}
-        {router.pathname === "/hospitals/[id]/gallery" && <GalleryCard />}
-      </div>
-    </div>
-  );
-};
-
-export default HospitalDetailPage;
-=======
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { DoctorsList, GalleryCard, ContactCard, HospitalDetailCard, Loading } from '../../../components/index';
-import { useGetHospitalByIdQuery } from '../../../store/hospital/hospital-detail-api';
+} from "@/components";import { useGetHospitalByIdQuery } from '../../../store/hospital/hospital-detail-api';
 
 const HospitalDetailPage = () => {
   const router = useRouter();
@@ -140,4 +74,3 @@ const HospitalDetailPage = () => {
 };
 
 export default HospitalDetailPage
->>>>>>> bbc53e3 (arrange folder structure)
