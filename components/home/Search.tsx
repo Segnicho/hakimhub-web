5c048729d3ac88d2b8fb3d591831bf58538c5685
Segnicho <segnidesta60@gmail.com>
2023-06-22 10:10:34 +0300

    make landing page responsive

--- components/home/Search.tsx
+++ components/home/Search.tsx
@@ -3,7 +3,7 @@ import { MdSend } from "react-icons/md";
 
 const Search: React.FC = () => {
   return (
-    <div className="flex items-center p-2 rounded shadow-md mt-5 ml-3 bg-[#EFF3F9] w-1/2 max-md:w-full">
+    <div className="flex items-center p-2 rounded shadow-md mt-5 ml-8 bg-[#EFF3F9] w-2/5 max-md:w-full">
       <input
         type="text"
         className="mr-2 p-2 outline-none w-full bg-inherit"
