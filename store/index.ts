8a776770910531b83f562a95a8063714ee4faf46
zerabruck muluneh <91604009+zerabruck@users.noreply.github.com>
2023-06-26 13:25:09 +0300

    implement state management for doctor detail  (#15)
    * update doctor detail
    
    * update text color
    
    * update file name
    
    * add state managment for doctor detail
    
    * update sotre configuration
    
    * update image file
    
    * update src in image component
    
    * import loading component
    
    ---------
    
    Co-authored-by: zerabruckmuluneh <zerabruck@a2sv.org>
    Co-authored-by: Segni Desta <88204792+Segnicho@users.noreply.github.com>

--- store/index.ts
+++ store/index.ts
@@ -1,16 +1,21 @@
+
 import { configureStore } from '@reduxjs/toolkit'
 import { hospitalDetailApi } from './../store/hospital/hospital-detail-api'
 import { hospitalsApi } from './features/hospital/hospitals-api'
+import { doctorDetail } from "@/slices/doctors/doctor-detail-api";
 
 export const store = configureStore({
   reducer: {
     [hospitalDetailApi.reducerPath]: hospitalDetailApi.reducer,
     [hospitalsApi.reducerPath]: hospitalsApi.reducer,
+    [doctorDetail.reducerPath]: doctorDetail.reducer
+
   },
 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(hospitalDetailApi.middleware)
     .concat(hospitalsApi.middleware)
+  .concat(doctorDetail.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
