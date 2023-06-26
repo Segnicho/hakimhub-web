ac8a3b3dba9602d921c66a4ebf84c960e22b34a5
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-06-26 16:24:56 +0300

    (feat: add overview)fetch overview and show sliders (#23)
    * fetch overview and show sliders
    
    * add state to overview

--- components/hospital-detail/Overview.tsx
+++ components/hospital-detail/Overview.tsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useRef } from "react";
 import "leaflet/dist/leaflet.css";
-import ServicesCard from "@/components/commons/ServicesCard";
+import ServicesSlide from "../commons/ServicesSlide";
+interface OverviewProps {
+  about: string | undefined;
+  services: string[] | undefined;
+}
+const Overview: React.FC<OverviewProps> = ({ about, services }) => {
+  const genericSummary =
+    "At Our Institution, we are committed to delivering exceptional healthcare services. Our experienced doctors and state-of-the-art facilities ensure that you receive the highest quality care With a focus on personalized treatment, our skilled doctors bring a wealth of knowledge and expertise to each patient's journey. We prioritize your well-being and strive to provide innovative solutions tailored to your needs. Trust is at the core of our institution. We have built a reputation for delivering successful outcomes and earning the confidence of our patients. From routine care to specialized treatments, we are here to serve you with compassion and dedication.";
+  if (about === undefined) {
+    about = genericSummary;
+  }
 
-const Overview = () => {
   const mapRef = useRef(null);
-  const numServices = 4;
-  const services = Array(numServices)
-    .fill(null)
-    .map((_, index) => <ServicesCard key={index} service={""} />);
   useEffect(() => {
     if (mapRef.current === null) {
       const L = require("leaflet");
@@ -26,31 +31,13 @@ const Overview = () => {
     <div className="p-5 space-y-4">
       <div className="bg-white rounded-md p-5 w-[95%] shadow-md">
         <h1 className="font-medium text-[30px]">About Us</h1>
-        <p className="py-5">
-          Black Lion (Tikur Anbessa) Specialized Hospital, School of Medicine,
-          College of Health Sciences, Addis Ababa University is the largest
-          referral hospital in Ethiopia. It was established in 1964, and is now
-          the main teaching center for both clinical and preclinical training of
-          most disciplines. It is also an institution where specialized clinical
-          services that are not available in other public or private
-          institutions
-        </p>
-        <p>
-          provide patient care in the hospital. In addition, almost all regional
-          and federal hospitals in Addis Ababa are affiliated to the School of
-          Medicine as clinical services and training sites.
-        </p>
+        <p className="py-5">{about}</p>
       </div>
       <div className="bg-white rounded-lg w-[95%] shadow-md z-10">
         <div id="map" className="h-96"></div>
       </div>
-      <div className="bg-white rounded-lg w-[95%] shadow-md z-10 flex">
-        {services.map((service, index) => (
-          <div className="w-[200px]" key={index}>
-            {service}
-          </div>
-        ))}
-      </div>
+      <h1>Our Services</h1>
+      {services && <ServicesSlide slideShow={4} services={services} />}
     </div>
   );
 };
