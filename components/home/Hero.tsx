7b433879eeeeea090d47d99d00bfff743d800599
Segnicho <segnidesta60@gmail.com>
2023-06-30 11:23:43 +0300

    change font and overlap

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -7,7 +7,7 @@ const Hero: React.FC = () => {
     <div className="flex flex-row justify-between font-semibold font-poppins">
       <div className="cols-span-1 justify-center flex flex-col max-md:text-3xl text-[50px] ">
         <h1>Making Your </h1>
-        <span className="font-bold text-[80px] -my-5">
+        <span className="font-bold text-[60px] max-md:my-2 md:text-[80px] ">
           Health<span className="text-primary">Care</span>
         </span>
         <h1>Options More Accessible</h1>
