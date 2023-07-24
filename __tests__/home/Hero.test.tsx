02f2291322921bb2cf945f724e33a3ab3650971f
Segnicho <segnidesta60@gmail.com>
2023-07-24 09:15:20 +0300

    restructure the test folder

--- __tests__/home/Hero.test.tsx
+++ __tests__/home/Hero.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import {Hero} from "../components";
+import {Hero} from "../../components";
 
 test("renders Hero component correctly", () => {
   const { getByText } = render(<Hero />);
