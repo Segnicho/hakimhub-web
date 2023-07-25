fe9cb8fb4be071243685f8009cf7c20d9b8583f7
Segnicho <segnidesta60@gmail.com>
2023-07-25 14:31:34 +0300

    remove unnecessary comments

--- __tests__/home/Service.test.tsx
+++ __tests__/home/Service.test.tsx
@@ -9,13 +9,10 @@ describe("Service Component", () => {
   it("should render the service details correctly", () => {
     render(<Service {...mockService} />);
 
-    // Ensure that the component renders the service description correctly
     expect(screen.getByText(mockService.description)).toBeInTheDocument();
 
-    // Ensure that the component renders the service image correctly
     expect(screen.getByTestId("service-image")).toBeInTheDocument();
 
-    // Ensure that the component renders the service name correctly
     expect(screen.getByText(mockService.service)).toBeInTheDocument();
   });
 });
