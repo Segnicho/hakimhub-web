bdc397110d60b8f3f171f6429ad950494cf85ae3
Tseganesh Yifru <88632885+TseganeshYifruBerta@users.noreply.github.com>
2023-07-26 16:53:26 +0300

    remove duplicate code (#47)
    Co-authored-by: Tseganesh Yifru <tseganeshyifruberta>

--- components/commons/HospitalsLoading.tsx
+++ components/commons/HospitalsLoading.tsx
@@ -1,18 +1,18 @@
 import HospitalLoading from "./HospitalLoading"
 
 const HospitalsLoading : React.FC = () => {
+    const Load = []
     return (
         <div className="min-h-screen">
-       <div>
-              <div>
-              <HospitalLoading />
-              <HospitalLoading />
-              <HospitalLoading />
-              <HospitalLoading />
-              </div>
+          <div>
+            <div>
+              {[...Array(4)].map((_, index) => (
+                <HospitalLoading key={index} />
+              ))}
+            </div>
           </div>
-      </div>
-    )
+        </div>
+      );
 }
 
 export default HospitalsLoading;
