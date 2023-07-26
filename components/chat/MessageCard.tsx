0c71de07e6e8ba4dcf0dd670f1a166d6992bc0ca
zerabruck muluneh <91604009+zerabruck@users.noreply.github.com>
2023-07-26 10:53:33 +0300

    clean chat feature code (#49)
    Co-authored-by: zerabruckmuluneh <zerabruck@a2sv.org>

--- components/chat/MessageCard.tsx
+++ components/chat/MessageCard.tsx
@@ -13,18 +13,18 @@ const MessageCard: React.FC = () => {
           return (
             <div key={index} className= {typeof(message) === 'string'?"flex items-center gap-4 ":"flex items-center  gap-4  max-md:justify-end max-md:flex-row-reverse"} >
               
-        <div  className={typeof(message) === 'string'? message ===  "An error occured while generating response try again"?  " bg-red-50 p-7 w-full ": " bg-[#f4f8ff] p-7  w-full  ":"bg-white  w-full p-7"}>
-            {
-            typeof message === 'string' 
-            ? message === "An error occured while generating response try again" ?<p className="max-w-[600px] md:ml-[25%] text-red-600 ">{ message }</p>: <p className="max-w-[600px] md:ml-[25%] ">{ message }</p>:
-            <div  className="max-w-[600px] md:ml-[25%] pl-1/2 ">
-              <p>{message.value.reply}</p>
-              {
-                message.value.institutions && <RecomendedHospitalList institutions = {message.value.institutions} />
-              }       
+            <div  className={typeof(message) === 'string'? message ===  "An error occured while generating response.Please try again"?  " bg-red-50 p-7 w-full ": " bg-[#f4f8ff] p-7  w-full  ":"bg-white  w-full p-7"}>
+                {
+                typeof message === 'string' 
+                ? message === "An error occured while generating response.Please try again" ?<p className="max-w-[600px] md:ml-[25%] text-red-600 ">{ message }</p>: <p className="max-w-[600px] md:ml-[25%] ">{ message }</p>:
+                <div  className="max-w-[600px] md:ml-[25%] pl-1/2 ">
+                  <p>{message.value.reply}</p>
+                  {
+                    message.value.institutions && <RecomendedHospitalList institutions = {message.value.institutions} />
+                  }       
+                </div>
+                }
             </div>
-             }
-        </div>
               </div>           
           )
         })
