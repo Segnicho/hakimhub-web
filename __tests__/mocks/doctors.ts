95c5d00409c8e3c762d3892483047b4269aed009
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-07-28 15:06:20 +0300

    add mock folder and datas (#55)

---
+++ __tests__/mocks/doctors.ts
@@ -0,0 +1,15 @@
+import { Doctor } from "@/types/chat/chat-types";
+const mockDoctors: Doctor[] = [
+  {
+    fullName: "John Doe",
+    about: "Experienced doctor",
+    gender: "Male",
+    email: "john.doe@example.com",
+    photoUrl: "https://example.com/john-doe.jpg",
+    yearsOfExperience: 10,
+    mainInstitutionId: "12345",
+    mainInstitutionName: "Example Hospital",
+    specialities: ["Cardiology"],
+    id: "6789",
+  },
+];
