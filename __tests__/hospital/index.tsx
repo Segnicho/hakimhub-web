585d11702098a7ef4df8b131884c81a1d94355e4
Segnicho <segnidesta60@gmail.com>
2023-07-29 18:01:12 +0300

    add tests for error causes.

--- __tests__/hospital/index.tsx
+++ __tests__/hospital/index.tsx
@@ -5,7 +5,6 @@ import { useGetHospitalsQuery } from "@/store/features/hospital/hospitals-api";
 import { hospitalsResponse } from "../mocks/hospitals";
 import { store } from "@/store";
 
-// Mock the useGetHospitalsQuery hook
 
 jest.mock("@/store/features/hospital/hospitals-api", () => ({
   ...jest.requireActual("@/store/features/hospital/hospitals-api"),
@@ -25,10 +24,8 @@ test("renders Hospitals Page with Correct Data of hospitals", async () => {
     </Provider>
   );
 
-  // Check if the HospitalFiltering component is rendered
   await screen.findAllByText("Filter Hospitals");
 
-  // Check if the HospitalsList component is rendered with the correct hospitals data
   await screen.findByText("Paulos");
   await screen.findByText("Yekatit");
 });
@@ -36,7 +33,6 @@ test("renders Hospitals Page with Correct Data of hospitals", async () => {
 
 
 test("renders loading state correctly", async () => {
-  // Mock the useGetHospitalsQuery hook with isLoading=true
   (useGetHospitalsQuery as jest.Mock).mockReturnValue({
     data:null,
     isLoading: true,
@@ -53,7 +49,6 @@ test("renders loading state correctly", async () => {
 });
 
 test("renders error state correctly", async () => {
-  // Mock the useGetHospitalsQuery hook with isError=true
   (useGetHospitalsQuery as jest.Mock).mockReturnValue({
     isLoading: false,
     isError: true,
@@ -65,13 +60,13 @@ test("renders error state correctly", async () => {
     </Provider>
   );
 
-  // Check if the error state is displayed
   await screen.findByText("Oops!");
+  await screen.findByText("Something went wrong.");
+  await screen.findByText("Back to Home");
 });
 
 
 test("renders NoData component when no hospitals data is available", async () => {
-  // Mock the useGetHospitalsQuery hook with an empty data array
   (useGetHospitalsQuery as jest.Mock).mockReturnValue({
     data: { value: [] },
     isLoading: false,
@@ -84,7 +79,6 @@ test("renders NoData component when no hospitals data is available", async () =>
     </Provider>
   );
 
-  // Check if the NoData component is displayed
     await screen.findByTestId("no-data");
     await screen.getByText("Seems like the hospital you're looking doesn't exist in our platform,");
     await screen.getByText("But if you want us to find you other alternatives based on your illness");
