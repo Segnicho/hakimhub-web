bd784adc43c68477d739f16842adfa0b89a67053
zelalemyetsedaw <zy94988@gmail.com>
2023-07-31 16:38:40 +0300

    test for loading component (#54)
    * test
    
    * test for docterdetail
    
    * fix some folderstructure issues

--- components/commons/Loading.tsx
+++ components/commons/Loading.tsx
@@ -1,7 +1,7 @@
 const LoadingPage: React.FC = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-primary"></div>
+    <div role="load" className="flex items-center justify-center h-screen">
+      <div role='load' data-testid="loading-spinner" className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-primary"></div>
     </div>
   );
 };
